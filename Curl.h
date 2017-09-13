6136f519259dc8423b4dbf0b7e270c4eb760c8a9
shahzadlone <shahzadlone@gmail.com>
2017-09-13 19:03:11 -0400

    Added Curl.h
    Added the Wrapper Curl.h Header File for Curlling Support

---
+++ Curl.h
@@ -0,0 +1,36 @@
+#ifndef CURL_H
+#define CURL_H
+
+#include <string>
+#include <curl/curl.h>
+#include <cstdlib>
+
+namespace curlcpp {
+
+    namespace libcurlwrapper {
+    
+        // Used for an Option inside CurlInitialization.
+        size_t CurlWriter( void * dataPointer,
+                           size_t size,
+                           size_t nmemb,
+                           std::string * writerData ); 
+
+        // Sets the Curling Options and Tells Where to Save the Result and Errors.
+        bool CurlInitialization( CURL *& conn,
+                                 const std::string & url,
+                                 std::string * errorStr, 
+                                 std::string * resultStoreBuffer ); 
+
+        // Curls the URL to Get the Result and then Cleans Up.
+        bool CurlAndClean( CURL *& conn,
+                           const std::string & url,
+                           std::string * errStr ) ;
+    
+    } // Closing Brace of namespace libcurlwrapper
+    
+    // Returns the Curled Output of the consumed url, in a String Form.
+    std::string Curl( const std::string & url );
+
+} // Closing Brace of namespace curlcpp
+
+#endif // End of Guard
