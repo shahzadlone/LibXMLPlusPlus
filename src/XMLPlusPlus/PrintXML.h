79ffbfdc79606ffbbedaf7f7a7e8e2529cc0d2a7
shahzadlone <shahzadlone@gmail.com>
2017-09-14 17:38:08 -0400

    Addded PrintXML's Header File
    Slightly Modified libxml++'s Printing Function Example, to Print Nodes

---
+++ src/XMLPlusPlus/PrintXML.h
@@ -0,0 +1,8 @@
+#include <libxml++/libxml++.h>
+
+namespace xmlhelp { 
+
+    // Function to help us print node if we ever watn to.
+    void PrintNode( const xmlpp::Node * node, unsigned int indentation = 0 ); 
+
+}
